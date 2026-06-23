4ba5ddc8fc4aa3be7481bfacb039fd26ec859f69
NIBIA RODRIGUEZ <rodrigueznibia44gmail.com>
2026-06-22 22:11:46 -0500

    Agregar clase Departamento y configurar conexión a PostgreSQL en AppHost

--- Practica_del_video_web.AppHost/AppHost.cs
+++ Practica_del_video_web.AppHost/AppHost.cs
@@ -1,3 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var dbserver = builder.AddPostgres("dbserver").WithLifetime(ContainerLifetime.Persistent);
+dbserver.WithPgAdmin(optiones =>
+optiones.WithHostPort(5555).WithLifetime(ContainerLifetime.Persistent)
+);
+var db = dbserver.AddDatabase("db");
+
+builder.AddProject<Projects.apiB>("api").WithExternalHttpEndpoints().WithReference(db).WaitFor(dbserver);
+
 builder.Build().Run();
